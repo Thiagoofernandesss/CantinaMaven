058096a50480f2ec369c604ac7b37988d10593b5
gabrieliribeiro <gabrielirt@hotmail.com>
2024-04-28 00:22:13 -0300

    Commit de segurança

--- target/generated-sources/annotations/model/bo/Carteirinha_.java
+++ target/generated-sources/annotations/model/bo/Carteirinha_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import model.bo.Cliente;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2024-04-27T23:22:45")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2024-04-28T00:19:00")
 @StaticMetamodel(Carteirinha.class)
 public class Carteirinha_ { 
 
--- target/generated-sources/annotations/model/bo/Contas_.java
+++ target/generated-sources/annotations/model/bo/Contas_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import model.bo.Compra;
 import model.bo.Venda;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2024-04-27T23:22:45")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2024-04-28T00:19:00")
 @StaticMetamodel(Contas.class)
 public class Contas_ { 
 
